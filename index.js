f249e1725922ff2c7e85fa1ac2a439f054401f70
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-09-21 14:00:57 -0500

    Completed adding the responses

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const server = require("./server.js");
 
-const port = "8000";
+// const port = "8000";
+const port = process.env.PORT;
 
 server.listen(port, () => {
   console.log(`API is running in port ${port}`);
--- routers/restaurantRouter.js
+++ routers/restaurantRouter.js
@@ -3,35 +3,41 @@ const axios = require("axios");
 const router = express.Router();
 
 //GET ALL ACTIONS
-router.get("/", async (req, res) => {
-  console.log("------IN API GET RESTAURANTS--------");
-  res.status(200).json({ message: "Restaurants" });
+router.get("/:latitude/:longitude/:radius/:limit", async (req, res) => {
+  //:latitude/:longitude/:radius/:limit
+  console.log("RESTAURANTS-ROUTER:: WITHIN RESTAURANTS /api/restaurants");
+  //var paramsValue = `term:"restaurants", latitude:"33.1601807", longitude:"-96.70983"`;
+  var radius = 5 * 1610;
+  var limit = 10;
+  radius = req.params.radius ? req.params.radius * 1610 : radius;
+  limit = req.params.limit ? req.params.limit : limit;
+  let paramsObj = {
+    term: "restaurants",
+    latitude: req.params.latitude,
+    longitude: req.params.longitude,
+    radius: radius,
+    limit: limit
+  };
   try {
-    const restaurants = await axios.get(
-      "https://api.yelp.com/v3/businesses/search",
-      {
-        headers: {
-          Authorization: `Bearer 0wtJSr_sSSCX8U-GbdmeKFYfipM8Xa2vNPn1GSbXe5B4Alhzp8sX64uvSTzAOOPquRWl54Cw6kugm6_TBD1Q2CaoX0uNWVUG6NOaLjpGhFhCxUbHoKlrq8DrXkOGXXYx`
-        },
-        params: {
-          term: "restaurants",
-          latitude: "33.1601807",
-          longitude: "-96.70983"
-        }
-      }
-    );
+    const restaurants = await axios.get(process.env.YELP_BASE_URL, {
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+      },
+      params: paramsObj
+    });
     console.log(restaurants.data.businesses.length);
-    // if (actions.length > 0) {
-    //   res.status(200).json(actions);
-    // } else {
-    //   res
-    //     .status(400)
-    //     .json({ ERROR_MESSAGE: "There are no actions to display" });
-    // }
+    res.status(200).json(restaurants.data);
+    if (restaurants.data.businesses.length > 0) {
+      res.status(200).json(restaurants.data.businesses);
+    } else {
+      res
+        .status(400)
+        .json({ ERROR_MESSAGE: "There are no restaurants to display" });
+    }
   } catch (error) {
-    // res.status(500).json({
-    //   ERROR_MESSAGE: "There was an error while retrieving the actions."
-    // });
+    res.status(500).json({
+      ERROR_MESSAGE: "There was an error while retrieving the restaurants."
+    });
     console.log(error);
   }
 });
